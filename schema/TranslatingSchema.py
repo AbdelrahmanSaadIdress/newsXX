941ecf4f75cf8175e0bd4d20924e4c4deb6db46f
AbdelrahmanSaadIdress <abdo200saad@gmail.com>
2026-04-30 22:46:12 +0300

    create the schemas

---
+++ schema/TranslatingSchema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import List, Literal
+
+class TranslatedStory(BaseModel):
+    translated_title: str = Field(..., min_length=5, max_length=300,
+                                    description="Suggested translated title of the news story.")
+    translated_content: str = Field(..., min_length=5,
+                                    description="Translated content of the news story.")
+    
